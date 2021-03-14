d85963e9577341822ef492bac3c8dfe77b660987
Emil Stryszawski <stryszawskiemil@gmail.com>
2021-03-14 22:38:18 +0100

    Swagger docs

--- src/main/java/com/example/springredditclone/config/SecurityConfig.java
+++ src/main/java/com/example/springredditclone/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.springredditclone.config;
 
 import com.example.springredditclone.security.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
